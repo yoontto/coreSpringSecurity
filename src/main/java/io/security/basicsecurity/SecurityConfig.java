81ce73d8996b68952579c51d569ede6c7c923d61
yoontto <jedney69@gmail.com>
2024-02-28 16:46:11 +0900

    스프링 시큐리티 7강/64강 - FormLogin 인증 - FormLogin 관련 설정 코드 적용 - Deprecated 코드들 현재 버전에 맞는 코드로 변경

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,6 +1,5 @@
 package io.security.basicsecurity;
 
-import jdk.jfr.Frequency;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,8 +14,44 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeRequests(req -> req.anyRequest().authenticated())
-                .formLogin(form -> form.permitAll());
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+
+                //.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+                .formLogin(form -> form
+                        //.loginPage("/loginPage")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/loginPage")
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+                        .loginProcessingUrl("/login_proc")
+                        /* 강의 버전 Handler (람다 이전)
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                System.out.println("authentication : " + authentication.getName());
+                                response.sendRedirect("/");
+                            }
+                        })
+                        .failureHandler(new AuthenticationFailureHandler() {
+                            @Override
+                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                System.out.println("exception : " + exception.getMessage());
+                                response.sendRedirect("/loginPage");
+                            }
+                        })
+                        */
+                        /* 람다 사용한 버전 */
+                        .successHandler((request, response, authentication) -> {
+                            //인증에 성공한 사용자 이름
+                            System.out.println("authentication : " + authentication.getName());
+                            response.sendRedirect("/");
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception : " + exception.getMessage());
+                            response.sendRedirect("/loginPage");
+                        })
+                        .permitAll());
+
 
         return http.getOrBuild();
     }
